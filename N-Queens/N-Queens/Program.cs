b5843d25a37c6b464a49d43cd6b4f395db671926
Doo <lfduh@yahoo.com.tw>
2022-11-02 10:31:30 +0800

    Update Program.cs

--- N-Queens/N-Queens/Program.cs
+++ N-Queens/N-Queens/Program.cs
@@ -14,6 +14,7 @@
                 ShowPattens( pattens );
 
             Console.WriteLine( "==End==" );
+            Console.ReadLine();
         }             
 
         static List<List<int>> GetNextPattens ( int _targetPattenSize, List<int> _currentPatten )
